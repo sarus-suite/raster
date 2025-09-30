561ee0feae59ac6028c1aeb44b4def1850dff3a2
Matteo Chesi <chesi@cscs.ch>
2025-09-30 18:20:26 +0200

    VCUE-1081 embedded json schema for EDF

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,15 @@ use toml::Table;
 use toml::Value;
 use toml::map::Map;
 use std::error::Error;
-//use std::path::Path;
-/*
+use std::path::Path;
+use std::ffi::OsStr;
 use serde::Deserialize;
 
 
 #[derive(Deserialize)]
 pub struct EDF {
-   base_environment: Option<BaseEnvironment>,
-   image: Option<String>,
+   _base_environment: Option<BaseEnvironment>,
+   _image: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -18,14 +18,33 @@ pub enum BaseEnvironment {
     TypeString(String),
     TypeVec(Vec<String>),
 }
-*/
 
 pub fn load(file_path: &str) -> Result<String, Box<dyn Error>> {
-  println!("Trying to read {file_path}");
+
+  // SD-67022 - prevent reading wrong file
+  let fp = Path::new(file_path);
+
+  let fname = match fp.file_name().and_then(OsStr::to_str) {
+      Some(name) => name,
+      None => return Err(format!("Cannot extract file name from {file_path}").into()),
+  };
+  
+  let ext = match fp.extension().and_then(OsStr::to_str) {
+      Some(x) => x,
+      None => return Err(format!("Cannot extract file extension from {file_path}").into()),
+  };
+
+  if ext != "toml" {
+      return Err(format!("File name {fname} doesn't end with .toml").into());
+  }
+
+  if ! fp.exists() {
+      return Err(format!("File {file_path} not found").into());
+  }
 
   let outstr = std::fs::read_to_string(file_path)?;
 
-  println!("This is its content:\n\n{outstr}\n");
+  //println!("This is its content:\n\n{outstr}\n");
 
   Ok(outstr)
 }
@@ -50,14 +69,14 @@ pub fn edf_parse(toml: Map<String, Value>) -> Result<Map<String, Value>, Box<dyn
     Ok(toml)
 }
 
-/*
+
 pub fn edf_parse2(input: &str) -> Result<EDF, Box<dyn Error>> {
     let edf: EDF = toml::from_str(input)?;
-    println!("base_environment: {}", edf.base_environment.ok_or("NULL")?);
-    println!("image: {}", edf.image.ok_or("NULL")?);
+    //println!("base_environment: {}", edf.base_environment.ok_or("NULL")?);
+    //println!("image: {}", edf.image.ok_or("NULL")?);
     Ok(edf)
 }
-*/
+
 pub fn edf_load(file_path: &str) -> Result<Map<String, Value>, Box<dyn Error>> {
 
     let _content = load(file_path)?;
@@ -66,14 +85,96 @@ pub fn edf_load(file_path: &str) -> Result<Map<String, Value>, Box<dyn Error>> {
     Ok(edf_map)
 }
 
-/*
 pub fn edf_load2(file_path: &str) -> Result<EDF, Box<dyn Error>> {
 
     let _content = load(file_path)?;
     let edf = edf_parse2(&_content)?; 
     Ok(edf)
 }
-*/
+
+pub fn validate(path: String) -> bool {
+
+    let path_str = path.as_str();
+
+    // Embedding schema file
+    let schema_content = include_str!("schema/edf.json");
+
+    // From: https://docs.rs/crate/jsonschema-for-toml/0.1.0/source/src/main.rs
+    // Embedded schema not needed at runtime
+    /*
+    let schema_path_str = "./src/schema/edf.json";
+
+    let schema_content = match std::fs::read_to_string(&schema_path_str) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Failed to read schema file: {}", schema_path_str);
+            return false;
+        }
+    };
+
+    let schema: serde_json::Value = match serde_json::from_str(&schema_content) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Failed to parse schema file: {}", schema_path_str);
+            return false;
+        }
+    };
+    */
+
+    let schema: serde_json::Value = match serde_json::from_str(&schema_content) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Failed to parse schema file");
+            return false;
+        }
+    };
+
+    // Create validator
+    let validator = match jsonschema::options().build(&schema) {
+        Ok(v) => v,
+        Err(error) => {
+            eprintln!("Schema is invalid. Error: {error}");
+            return false;
+        }
+    };
+    
+    let toml_content = match load(path_str) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("{}", e);
+            return false;
+        },
+    };
+
+    let toml_value = match toml::from_str(&toml_content) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{}", e);
+            return false;
+        },
+    };
+
+    let mut has_errors = false;
+    let mut errors = validator.iter_errors(&toml_value);
+
+    if let Some(first) = errors.next() {
+        has_errors = true;
+        println!("{path_str} is an INVALID EDF file.");
+        eprintln!("\nErrors:");
+        eprintln!("1. {first}");
+        for (i, error) in errors.enumerate() {
+            eprintln!("{}. {error}", i + 2);
+        }
+    } else {
+        println!("{path_str} is a valid EDF file");
+    }
+
+    if has_errors {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 #[cfg(test)]
 mod tests {
