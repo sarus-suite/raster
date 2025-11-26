aeebaa594e5166a45a63b081567d592d13a086b4
Gwangmu Lee <gwangmu.lee@cscs.ch>
2025-11-26 15:34:46 +0100

    Sanitize the string to be evaluated by Bash (no execution and redirection)

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::process::Command;
 
@@ -17,14 +18,20 @@ fn expand_vars_string_with_env(
     input: String,
     env: &HashMap<String, String>,
 ) -> SarusResult<String> {
-    /*
-     * Running bash here, as a unprivileged user, I suppose.
-     * Probably there is a security need for vetting:
-     * * running user (?)
-     * * input (?)
-     * * env (?)
-     */
+    // Ban any strings that will attempt to execute something upon evaluation.
+    let re_banned = Regex::new(r#"([^\\]|^)(\$\(|`|;|")"#).unwrap();
+    if re_banned.is_match(&input) {
+        return Err(SarusError {
+            code: 18,
+            file_path: None,
+            msg: String::from(format!("cannot expand string {input}, invalid string")),
+        });
+    }
+
+    // Evaluate 'input' in a restricted shell.
+    // This will block any redirection attempts, among other things.
     let output = Command::new("bash")
+        .arg("-r")
         .arg("-c")
         .arg(format!("echo -n \"{}\"", &input))
         .env_clear()
@@ -95,4 +102,51 @@ pub fn expand_vars_vec(
         newv.push(expand_vars_string(s, env)?);
     }
     return Ok(newv);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check_expand_vars_string(
+        input: &str,
+        expected: &str
+    ) -> bool {
+        let mut env = HashMap::new();
+        env.insert("XXX".to_string(), "111".to_string());
+        match expand_vars_string_with_env(input.to_string(), &env) {
+            Ok(s) => {
+                println!("{}", s);
+                return s == expected;
+            }
+            Err(_) => return false,
+        }
+    }
+
+    #[test]
+    fn expand_vars_normal_strs() {
+        assert!(check_expand_vars_string(r#"xxx-$XXX-xxx"#, r#"xxx-111-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-${XXX}-xxx"#, r#"xxx-111-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-${XXX:1}-xxx"#, r#"xxx-11-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-${YYY:-222}-xxx"#, r#"xxx-222-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-${YYY}-xxx"#, r#"xxx--xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-\$(XXX)-xxx"#, r#"xxx-$(XXX)-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-$\(XXX)-xxx"#, r#"xxx-$\(XXX)-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx-\`XXX\`-xxx"#, r#"xxx-`XXX`-xxx"#));
+        assert!(check_expand_vars_string(r#"xxx--xxx\;"#, r#"xxx--xxx\;"#));
+        assert!(check_expand_vars_string(r#"\"xxx--xxx\""#, r#""xxx--xxx""#));
+    }
+
+    #[test]
+    fn expand_vars_banned_strs() {
+        assert!(!check_expand_vars_string(r#"xxx-$(XXX)-xxx"#, ""));
+        assert!(!check_expand_vars_string(r#"xxx-`XXX`-xxx"#, ""));
+        assert!(!check_expand_vars_string(r#"xxx-"-xxx"#, ""));
+        assert!(!check_expand_vars_string(r#"xxx--xxx;"#, ""));
+        assert!(!check_expand_vars_string(r#"$(XXX)xxx--xxx"#, ""));
+        assert!(!check_expand_vars_string(r#"`XXX`xxx--xxx"#, ""));
+        assert!(!check_expand_vars_string(r#""xxx--xxx"#, ""));
+        assert!(!check_expand_vars_string(r#"; xxx--xxx"#, ""));
+        assert!(!check_expand_vars_string(r#"" >/tmp/file"#, ""));
+    }
 }
